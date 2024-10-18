57c8b8263bf2727649a23600241e04897937d10d
AyushMahawar <ayushmahawar60@gmail.com>
2024-10-18 22:26:40 +0530

    hosted url updated

--- web/src/webconst.tsx
+++ web/src/webconst.tsx
@@ -1,3 +1,3 @@
-export const baseurl: string = "http://localhost:8080/api/v1";
-// export const baseurl: string =
-//   "https://testimonial-backend-gio8.onrender.com/api/v1";
+// export const baseurl: string = "http://localhost:8080/api/v1";
+export const baseurl: string =
+  "https://testimonial-backend-gio8.onrender.com/api/v1";
