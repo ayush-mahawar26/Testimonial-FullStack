174e1000abfd359e2b4cd436d44bbac8bb43dcca
AyushMahawar <ayushmahawar60@gmail.com>
2024-10-27 18:45:11 +0530

    new review

--- testimonial-view/src/component/Avatar.tsx
+++
@@ -1,7 +0,0 @@
-export function Avatar({ name }: { name: string }) {
-  return (
-    <div className="text-slate-100 rounded-full bg-slate-600 px-4 flex items-center justify-center">
-      <p className="">{name.substring(0, 2).toUpperCase()}</p>
-    </div>
-  );
-}
