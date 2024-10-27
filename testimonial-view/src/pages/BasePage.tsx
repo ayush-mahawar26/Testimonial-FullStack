011eb0e6b3bbcdf4d512c064d95f4be1b031d177
AyushMahawar <ayushmahawar60@gmail.com>
2024-10-27 18:03:10 +0530

    test

--- testimonial-view/src/pages/BasePage.tsx
+++ testimonial-view/src/pages/BasePage.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function BasePage() {
+  const nav = useNavigate();
+
+  useEffect(() => {
+    nav("/test");
+  });
   // const param = useParams();
 
   // const projectId = param["id"];
