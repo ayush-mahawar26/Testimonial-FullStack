367f452c5536a1d60e94975010cd9ccb5607b75b
AyushMahawar <ayushmahawar60@gmail.com>
2024-11-01 18:32:48 +0530

    corserror

--- backend-new/src/index.ts
+++ backend-new/src/index.ts
@@ -15,7 +15,7 @@ const app: Express = express();
 const corsOptions = {
   origin: "*",
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
